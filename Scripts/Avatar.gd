ab2ebe666c8cba84ccdc43d00c2571ef32666a29
gregorhcs <gregor.soennichsen@outlook.com>
2026-01-31 17:40:12 +0100

    Speech & radial jitter progress

--- Scripts/Avatar.gd
+++ Scripts/Avatar.gd
@@ -1,17 +1,27 @@
 extends Node2D
 
 @export var AnimatedAvatarSprite: AnimatedSprite2D
+@export var SpeechText: RichTextLabel
 
 var current_goal = Vector2(0, 0)
 var is_moving = false
 
 func move_to(position: Vector2) -> void:
 	current_goal = position
+	set_text("Okay..")
+
+func set_text(text: String) -> void:
+	SpeechText.text = text
+	await get_tree().create_timer(text.length() * 0.2).timeout
+	clear_text()
+
+func clear_text() -> void:
+	SpeechText.text = "";
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	current_goal = position
-
+	clear_text()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
--- Scripts/GameDirector.gd
+++ Scripts/GameDirector.gd
@@ -1,12 +1,17 @@
 extends Control
 
 @export var AvatarControl : Node
-@export var WalkableArea : Node
+@export var Mask : TextureRect
 
 @export var ScreenUIContainer : TextureRect
 @export var Screens : Array[Texture2D]
 
 var CurrentScreen = 0;
+var CurrentInsanity = 0;
+
+func increase_insanity(amount: int) -> void:
+	CurrentInsanity += amount;
+	Mask.material.set_shader_parameter("CRACK_profile", CurrentInsanity / 100.0)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -19,12 +24,13 @@ func _process(delta: float) -> void:
 func _on_background_image_gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.pressed:
 		AvatarControl.move_to(event.position)
-
+		increase_insanity(1)
 
 func _update_screen(new_screen: int) -> void:
 	if new_screen != CurrentScreen:
 		ScreenUIContainer.texture = Screens[CurrentScreen]
 		CurrentScreen = new_screen
+		increase_insanity(3)
 
 func _on_sc_level_change_left_triggered() -> void:
 	_update_screen((CurrentScreen  - 1) % Screens.size())
